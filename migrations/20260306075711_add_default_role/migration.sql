4c20fd823c67ca4bbd7998c40afc27f4f3ed1a43
Adrian Herscu <adrian.herscu@gmail.com>
2026-03-06 10:05:35 +0200

    Add default role to User model and create migration for existing records

---
+++ migrations/20260306075711_add_default_role/migration.sql
@@ -0,0 +1,12 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "role" TEXT NOT NULL DEFAULT 'student'
+);
+INSERT INTO "new_User" ("id", "role") SELECT "id", "role" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
